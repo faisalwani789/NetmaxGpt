97a02ab9972317216ef1f7e223618ea9875f35a7
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-03-31 09:25:16 +0530

    selected 0 vid

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -14,7 +14,7 @@ const dispatch=useDispatch()
     // useGetMovieVideos(mainMovie.id, addTrailerVideo)
     if (!nowPlayingMovies) return //if movies is null till movies is fetched from the store
     // console.log("movies fetched"+JSON.stringify(movies[0]))
-    const mainMovie = nowPlayingMovies[0]
+    const mainMovie = nowPlayingMovies[5]
     dispatch(setMainPlayingId(trailerVideo?.key))
     // const youtube_key=
     return (
